92a4b06ee0e98633e3f8a2617515c6bd85354b7c
mdbulbulhasan <bulbulhasan095@gmail.com>
2025-08-17 14:09:42 +0600

    fix the whychoose us section

--- src/pages/Home/WhyChooseUs/WhyChooseUs.jsx
+++ src/pages/Home/WhyChooseUs/WhyChooseUs.jsx
@@ -43,7 +43,7 @@ const reviews = [
 const WhyChooseUs = () => {
   return (
     <section className="py-12 bg-white">
-      <div className="max-w-6xl mx-auto px-4">
+      <div className="px-4">
         <h2 className="text-3xl md:text-4xl font-bold text-green-800 mb-12 text-center">
           💚 Why Choose Bazaroid?
         </h2>
